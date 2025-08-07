af160a5b59f495ee0a49f488ad4d58453e2a310c
Prince <princecultura10@gmail.com>
2025-08-07 19:16:56 +0800

    Create 1st.cpp

---
+++ 1st.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main () {
+
+    int Num1, Num2;
+    string sum = "Result";
+    cout << "Enter First Number: ";
+    cin >> Num1
+    cout << "Enter second Number: ";
+    cin >> Num2
+    cout >> "Result: " << num1 + num2;
+
+    return 0;
+}
