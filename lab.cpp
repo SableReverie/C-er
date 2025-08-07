08f9cee2de1249515ac499e0f6789c84cc70e6d5
Prince <princecultura10@gmail.com>
2025-08-07 19:17:17 +0800

    Create lab.cpp

---
+++ lab.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main (){
+    
+    // Personal Profile
+    cout << left;
+    cout << setw(15) << "Name: " << "Prince" << endl;
+    cout << setw(15) << "Age: " << "18" << endl;
+    cout << setw(15) << "Course: " << "CpE" << endl;
+    cout << setw(15) << "Year Level: " << "1st Year" << endl;
+    cout << setw(15) << "Hobby: " << "Mobile Legends" << endl;  
+    
+    return 0;
+}
